126f2b2621096d43498ae062cf08cb2f561d005d
06yasue <101648951+06yasue@users.noreply.github.com>
2026-04-24 22:53:42 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,18 +10,22 @@ export default async function RootLayout({ children }) {
   const { data: settings } = await supabase.from('settings').select('head_script').eq('id', 1).single();
 
   return (
-    <html lang="id">
+    <html lang="en">
       <head>
+        {/* 1. CSS BOOTSTRAP */}
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-        {/* LINK IKON WAJIB DI SINI BIAR GAK RUSAK (BUKAN DI CSS) */}
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        
+        {/* 2. GOOGLE FONTS (Jost) & MATERIAL ICONS (Wajib di sini biar gak keblokir) */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         
         {settings?.head_script && (
           <div dangerouslySetInnerHTML={{ __html: settings.head_script }} />
         )}
       </head>
       <body>
-        {/* Hapus div main-wrapper, biarkan children langsung */}
         {children}
         <div id="notification-bar" className="custom-notification"></div>
       </body>
