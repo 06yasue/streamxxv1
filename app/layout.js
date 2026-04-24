21259f4541ae8af37b3d7781119b304990acaec8
06yasue <101648951+06yasue@users.noreply.github.com>
2026-04-24 22:44:17 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,14 +13,16 @@ export default async function RootLayout({ children }) {
     <html lang="id">
       <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+        {/* LINK IKON WAJIB DI SINI BIAR GAK RUSAK (BUKAN DI CSS) */}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        
         {settings?.head_script && (
           <div dangerouslySetInnerHTML={{ __html: settings.head_script }} />
         )}
       </head>
       <body>
-        <div className="main-wrapper">
-          {children}
-        </div>
+        {/* Hapus div main-wrapper, biarkan children langsung */}
+        {children}
         <div id="notification-bar" className="custom-notification"></div>
       </body>
     </html>
